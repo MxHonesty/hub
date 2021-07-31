1ca5ba1ebc1fc1392be94840b410105ffb695aec
Stelian <stoianstelian33@gmail.com>
2021-08-01 01:47:37 +0300

    add google authentification

---
+++ auth.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
+)
+
+// Handler for authentification.
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, err := r.Cookie("auth")
+	if err == http.ErrNoCookie {
+		// Not authenticated
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		return
+	}
+	if err != nil {
+		// Other error
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	h.next.ServeHTTP(w, r)
+}
+
+// Returns a http.Handler decorated in authHandler.
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
+
+// loginHandler handles the third-party logins.
+// /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	if len(segs) < 4 {
+		w.Write([]byte("Provider not found"))
+		return
+	}
+
+	action := segs[2]
+	provider := segs[3]
+
+	switch action {
+	case "login":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to get provider "+
+				"%s: %s", provider, err), http.StatusBadRequest)
+			return
+		}
+		loginUrl, err := provider.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to GetBeginAuthURL "+
+				"for %s: %s", provider, err), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", loginUrl)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+
+	case "callback":
+		providerO, err := gomniauth.Provider(provider)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to get provider "+
+				"%s: %s", provider, err), http.StatusBadRequest)
+			return
+		}
+		creds, err := providerO.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to complete auth for "+
+				"%s: %s", provider, err), http.StatusInternalServerError)
+			return
+		}
+		user, err := providerO.GetUser(creds)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to get user from "+
+				"%s: %s", provider, err), http.StatusInternalServerError)
+			return
+		}
+
+		authCookieValue := objx.New(map[string]interface{}{
+			"name": user.Name(),
+		}).MustBase64()
+		http.SetCookie(w, &http.Cookie{
+			Name:  "auth",
+			Value: authCookieValue,
+			Path:  "/",
+		})
+		w.Header().Set("Location", "/chat")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Auth action %s not supported", action)
+	}
+}
--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 // Represents a single template
@@ -23,18 +28,37 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 
 func main() {
-	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	var addr = flag.String("addr", ":3000", "The addr of the application.")
 	flag.Parse()
+
+	err := godotenv.Load("hub.env") // Loads hub.env file.
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	clientId := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	securityKey := os.Getenv("SECURITY_KEY")
+
+	gomniauth.SetSecurityKey(securityKey)
+	gomniauth.WithProviders(google.New(clientId, clientSecret,
+		"http://localhost:3000/auth/callback/google"))
 
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	go r.run()
 
